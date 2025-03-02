1157aeb8d858fbb073a33e42c0930352995b33d6
janki// <janki20027@gmail.com>
2025-03-02 23:09:55 +0530

    Pointers

--- Pointers/print_an_array_using_pointer.cpp
+++ Pointers/print_an_array_using_pointer.cpp
@@ -3,6 +3,10 @@ using namespace std;
 int main(){
     int arr[]={1,2,3,4,5,6,7,8,9,10};
     int n=sizeof(arr)/sizeof(arr[0]);
+    int *p=arr;
 
-    
+    for(int i=0;i<n;i++){
+        cout<<p[i]<<" ";
+    }
+    return 0;
 }
